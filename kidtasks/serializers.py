7918b47c2a0530e6d1c911586317c025b3e7dadd
Reich.arm <rmariasp@puce.edu.ec>
2024-07-31 16:21:09 -0500

    Changes in the modell

--- kidtasks/serializers.py
+++ kidtasks/serializers.py
@@ -4,4 +4,5 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ['id', 'description', 'creation_date', 'is_completed', 'completion_date', 'consecutive_days']
+        fields = ['id', 'description', 'creation_date', 'completed']
+        read_only_fields=['creation_date']
